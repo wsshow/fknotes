7412c9c8bbd65b7b2dfdabf88c3c1c252e72cdb2
ws <wsshow@foxmail.com>
2026-07-23 11:59:10 +0800

    refactor: 移除旧附件原生通道

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,29 +12,5 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    let importChannel = FlutterMethodChannel(
-      name: "fknotes/attachment_import",
-      binaryMessenger: engineBridge.applicationRegistrar.messenger()
-    )
-    importChannel.setMethodCallHandler { call, result in
-      guard call.method == "availableStorageBytes" else {
-        result(FlutterMethodNotImplemented)
-        return
-      }
-      do {
-        let attributes = try FileManager.default.attributesOfFileSystem(
-          forPath: NSHomeDirectory()
-        )
-        result((attributes[.systemFreeSize] as? NSNumber)?.int64Value)
-      } catch {
-        result(
-          FlutterError(
-            code: "storage_capacity_unavailable",
-            message: error.localizedDescription,
-            details: nil
-          )
-        )
-      }
-    }
   }
 }
